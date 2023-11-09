fc0d15ce9e37bd5c094d54c5ade774926562c998
Robert Odhiambo <odhizrober254@gmail.com>
2023-11-09 10:24:44 +0000

    to dict

--- models/base_model.py
+++ models/base_model.py
@@ -10,11 +10,24 @@ class BaseModel:
     """
     Base model class that will be inherited from while creating instances
     """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Instantiates the BaseModel class"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs:
+                if key == 'created_at' or key == 'updated_at':
+                    setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
+                elif key != '__class__':
+                    setattr(self, key, value)
+            if 'id' not in kwargs:
+                self.id = str(uuid.uuid4())
+            if 'created_at' not in kwargs:
+                self.created_at = datetime.now()
+            if 'updated_at'not in kwargs:
+                self.updated_at = datetime.now()
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """
@@ -24,7 +37,7 @@ class BaseModel:
 
     def save(self):
         """ Saves the instance created by updated the updated_at field """
-        self.pdated_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         """ returns a dictionary containing all keys/values of __dict__ of the instance """
